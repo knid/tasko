c03f816770792d8b425f0f2d57f6dcb8bfdd2c69
Sinan Kanidagli <sinan@knid.me>
2024-04-04 21:12:05 +0300

    fix(tasko): return values

--- tasko.plugin.zsh
+++ tasko.plugin.zsh
@@ -1,4 +1,4 @@
-function tasko_print_help() {
+function _tasko_print_help() {
   echo
   echo "Usage: tasko {TASK} {ROW}"
   echo
@@ -8,27 +8,31 @@ function tasko () {
   if [[ $# -ne 2 ]]; then
     echo 'Error: Too many/few arguments, expecting two.' >&2
     tasko_print_help
-    return
+    return 1
   fi
 
-  annotation_desc=$(task _get $1.annotations.$2.description 2>/dev/null)
+  local annotation_desc=$(task _get $1.annotations.$2.description 2>/dev/null)
   if [[ $? -ne 0 ]]; then
     echo "Error: Task or row not found." >&2
-    return
+    return 1
   fi
 
   if [[ $annotation_desc == "" ]]; then
     echo "Error: No text found" >&2
-    return
+    return 1
   fi
 
   echo "Open $annotation_desc? [y/n]"
   read answer
   if [[ $answer == y* ]] || [[ $answer == Y* ]]; then
     if [[ $(uname -s) == "Linux" ]]; then
-      xdg-open $annotation_desc &
+      xdg-open $annotation_desc
+      return 0
     else
-      open $annotation_desc &
+      open $annotation_desc
+      return 0
     fi
+    return 1
   fi
+  return 0
 }
